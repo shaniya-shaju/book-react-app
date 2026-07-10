55a113725a705cabde9dc22cfc2b5e4968d04ecf
shaniya-shaju <shaniyashaju123@gmail.com>
2026-07-10 11:40:28 +0530

    updated

--- src/components/ViewBook.jsx
+++ src/components/ViewBook.jsx
@@ -1,8 +1,73 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navigation from './Navigation'
 
 
 const BookView = () => {
+    const [data,changeData]=useState(
+        [
+            {
+              "name": "The Alchemist",
+              "author": "Paulo Coelho",
+              "price": "₹399",
+              "avatar": "https://images-na.ssl-images-amazon.com/images/I/71aFt4+OTOL.jpg"
+            },
+            {
+              "name": "Atomic Habits",
+              "author": "James Clear",
+              "price": "₹599",
+              "avatar": "https://images-na.ssl-images-amazon.com/images/I/91bYsX41DVL.jpg"
+            },
+            {
+              "name": "Rich Dad Poor Dad",
+              "author": "Robert T. Kiyosaki",
+              "price": "₹450",
+              "avatar": "https://images-na.ssl-images-amazon.com/images/I/81bsw6fnUiL.jpg"
+            },
+            {
+              "name": "Think and Grow Rich",
+              "author": "Napoleon Hill",
+              "price": "₹349",
+              "avatar": "https://images-na.ssl-images-amazon.com/images/I/71UypkUjStL.jpg"
+            },
+            {
+              "name": "The Psychology of Money",
+              "author": "Morgan Housel",
+              "price": "₹499",
+              "avatar": "https://images-na.ssl-images-amazon.com/images/I/71g2ednj0JL.jpg"
+            },
+            {
+              "name": "Ikigai",
+              "author": "Héctor García",
+              "price": "₹399",
+              "avatar": "https://images-na.ssl-images-amazon.com/images/I/81l3rZK4lnL.jpg"
+            },
+            {
+              "name": "The Power of Your Subconscious Mind",
+              "author": "Joseph Murphy",
+              "price": "₹299",
+              "avatar": "https://images-na.ssl-images-amazon.com/images/I/71sBtM3Yi5L.jpg"
+            },
+            {
+              "name": "The 5 AM Club",
+              "author": "Robin Sharma",
+              "price": "₹550",
+              "avatar": "https://images-na.ssl-images-amazon.com/images/I/71zytzrg6lL.jpg"
+            },
+            {
+              "name": "Deep Work",
+              "author": "Cal Newport",
+              "price": "₹525",
+              "avatar": "https://images-na.ssl-images-amazon.com/images/I/71QKQ9mwV7L.jpg"
+            },
+            {
+              "name": "The Hobbit",
+              "author": "J.R.R. Tolkien",
+              "price": "₹699",
+              "avatar": "https://images-na.ssl-images-amazon.com/images/I/91b0C2YNSrL.jpg"
+            }
+          ]
+
+    )
   return (
     <div>
         <center><h1>View Book</h1></center>
@@ -12,167 +77,25 @@ const BookView = () => {
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
                     <div className="row g-3">
-                        <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+                        {data.map(
+                            (value,indedx) =>{
+                                return (
+                                    <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
 
                             <div class="card" >
-                            <div class="card-body">
-                                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiJWvdz-O3QHPR0-7JXF29YJMQ6tc7ZJSUaUImcWTMOQ&s=10" class="card-img-top" alt="The Alchemist" height="400"/>
-                                <h5 class="card-title">All is well</h5>
-                                <p class="card-text"><strong>Author:</strong> Lisa James</p>
-                                <p class="card-text"><strong>Price: ₹399</strong></p>
-                                <a href="#" class="btn btn-primary">Add to Cart</a>
+                                <div class="card-body">
+                                    <img src={value.avatar} class="card-img-top" alt="The Alchemist" height="400"/>
+                                    <h5 class="card-title">{value.name}</h5>
+                                    <p class="card-text"><strong>Author:</strong> {value.author}</p>
+                                    <p class="card-text"><strong>{value.price}</strong></p>
+                                    <a href="#" class="btn btn-primary">Add to Cart</a>
+                                </div>
                             </div>
-                        </div>
-
-
-                        </div>
-                        <div className="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
-
-                        <div class="card" >
-                            <div class="card-body">
-                                <img src="https://m.media-amazon.com/images/I/71tbalAHYCL.jpg" class="card-img-top" alt="Ikigai" height="400"/>
-                                <h5 class="card-title">Ikigai</h5>
-                                <p class="card-text"><strong>Author:</strong> Héctor García</p>
-                                <p class="card-text"><strong>Price: ₹450</strong></p>
-                                <a href="#" class="btn btn-primary">Add to Cart</a>
-                            </div>
-                        </div>
-
-
-
-                        </div>
-                        <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
-
-                        <div class="card" >
-                        <div class="card-body">
-                            <img src="https://m.media-amazon.com/images/I/81wgcld4wxL.jpg" class="card-img-top" alt="Atomic Habits" height="400"/>
-                            <h5 class="card-title">Atomic Habits</h5>
-                            <p class="card-text"><strong>Author:</strong> James Clear</p>
-                            <p class="card-text"><strong>Price: ₹599</strong></p>
-                            <a href="#" class="btn btn-primary">Add to Cart</a>
-                        </div>
+                        </div>   
+                                )
+                            }
+                        )}
                     </div>
-
-
-
-                        </div>
-                         <div className="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
-
-                            <div class="card">
-                                
-                                <div class="card-body">
-                                    <img src="https://i.pinimg.com/1200x/a2/f1/ac/a2f1ac4c7001cb25f2d6fbbc33e79e48.jpg" class="card-img-top" alt="..." height="350"/>
-                                    <h5 class="card-title">The Alchiemist</h5>
-                                    <p>AUTHOR: Paulo coelo</p>
-                                    <p class="card-text"><strong>Price: ₹899</strong></p>
-                                    <a href="#" class="btn btn-primary">Add to Cart</a>
-                                </div>
-                        </div>
-
-
-
-                        </div>
-                         <div className="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
-
-                            <div class="card">
-                                
-                                <div class="card-body">
-                                    <img src="https://i.pinimg.com/736x/3a/14/1e/3a141ec359673feb4dad6e64d92b1e0c.jpg" class="card-img-top" alt="..." height="350"/>
-                                    <h5 class="card-title">It end with us</h5>
-                                    <p>AUTHOR: Collen Hoover</p>
-                                    <p class="card-text"><strong>Price: ₹899</strong></p>
-                                    <a href="#" class="btn btn-primary">Add to Cart</a>
-                                </div>
-                                
-                        </div>
-                        
-
-
-
-                        </div>
-                        <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
-
-                            <div class="card">
-                                
-                                <div class="card-body">
-                                    <img src="https://i.pinimg.com/736x/fd/a0/a9/fda0a96c1288877b416f9f7618a87df2.jpg" class="card-img-top" alt="..."height="350"/>
-                                    <h5 class="card-title">How to steal a heart</h5>
-                                    <p>AUTHOR:YOU</p>
-                                    <p class="card-text"><strong>Price: ₹499</strong></p>
-                                    <a href="#" class="btn btn-primary">Add to Cart</a>
-                                </div>
-                                </div>
-
-
-                        </div>
-                        <div className="col col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
-
-                        <div class="card">
-                                
-                                <div class="card-body">
-                                    <img src="https://i.pinimg.com/736x/af/44/80/af4480b2d95058a9c6790b45d0e05d13.jpg" class="card-img-top" alt="..."height="400"/>
-                                    <h5 class="card-title">The secret garden</h5>
-                                    <p>AUTHOR:Hogson burnett</p>
-                                    <p class="card-text"><strong>Price: ₹349</strong></p>
-                                    <a href="#" class="btn btn-primary">Add to Cart</a>
-                                </div>
-                        </div>
-
-
-
-                        </div>
-                        <div className="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
-
-                            <div class="card" >
-                            <div class="card-body">
-                                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4NJ-Dn274vVN0TeNHw8XCRZL_poAzV9VNI7r6iIZgDw&s" class="card-img-top" alt="Wings of Fire" height="400"/>
-                                <h5 class="card-title">Wings of Fire</h5>
-                                <p class="card-text"><strong>Author:</strong> A.P.J. Abdul Kalam</p>
-                                <p class="card-text"><strong>Price: ₹350</strong></p>
-                                <a href="#" class="btn btn-primary">Add to Cart</a>
-                            </div>
-                        </div>
-
-
-
-                        </div>
-                        <div className="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
-
-                            <div class="card" >
-                            <div class="card-body">
-                                <img src="https://m.media-amazon.com/images/I/81YOuOGFCJL.jpg" class="card-img-top" alt="Harry Potter" height="400"/>
-                                <h5 class="card-title">Harry Potter</h5>
-                                <p class="card-text"><strong>Author:</strong> J.K. Rowling</p>
-                                <p class="card-text"><strong>Price: ₹699</strong></p>
-                                <a href="#" class="btn btn-primary">Add to Cart</a>
-                            </div>
-                        </div>
-
-
-
-                        </div>
-                        
-                        
-                        <div className="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
-
-                            <div class="card" >
-                            <div class="card-body">
-                                <img src="https://m.media-amazon.com/images/I/41SH-SvWPxL.jpg" class="card-img-top" alt="Clean Code" height="400"/>
-                                <h5 class="card-title">Clean Code</h5>
-                                <p class="card-text"><strong>Author:</strong> Robert C. Martin</p>
-                                <p class="card-text"><strong>Price: ₹950</strong></p>
-                                <a href="#" class="btn btn-primary">Add to Cart</a>
-                            </div>
-                        </div>
-
-
-
-                        </div>
-                        
-                    </div>
-
-
-
                 </div>
             </div>
         </div>
